22229eca5706ebf0c3e150dbb15a6cc4eb2fc2ff
davidkrauza <david@dockcreek.com>
2016-12-17 12:07:22 -0500

    Initial SQL example for unit 03

---
+++ unit-03/sql/us_flights.sql
@@ -0,0 +1,23 @@
+--SQL querying us_flights and flight_revenue dataset on Mode Analytics 
+select
+  f.unique_carrier,
+  f.dest,
+  sum(fr.cargo_rev) as total_cargo_rev,
+  sum(fr.first_class_rev) as total_1st_class_rev,
+  sum(fr.business_class_rev) as total_biz_class_rev,
+  sum(fr.coach_rev) as total_coach_rev
+from
+  tutorial.us_flights as f
+  join tutorial.flight_revenue as fr
+    on f.dest = fr.destination_airport
+where
+  f.actual_elapsed_time >= 138
+group by
+  f.unique_carrier,
+  f.dest
+order by
+  f.unique_carrier,
+  total_cargo_rev desc,
+  total_coach_rev desc,
+  f.dest
+limit 120;
